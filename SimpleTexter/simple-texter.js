4c28e479300ce48b18413417c848e9cb7acf7f51
Keith Chester <kchester@gmail.com>
2016-08-06 16:33:31 -0500

    features: Added simple-texter example to folder

--- SimpleTexter/simple-texter.js
+++ SimpleTexter/simple-texter.js
@@ -3,15 +3,34 @@ var bodyparser = require('body-parser');
 var app = express();
 
 //Twilio requires keys, which I hid in auth.json, which won't be included in the git repos for obv reasons
-var auth = require('credentials.json');
+var auth = require('./auth.json');
 var twilio = require('twilio')(auth.accountSID, auth.authToken);
+
+//JSON file contacts = JS object, keys are names matching to textTo, values are resulting #s
+var numbers = require('./numbers.json');
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use(function(req, res){
 	console.log(req.body);
-	res.status(200).end();
+	twilio.sendMessage(
+		{
+			to: '9089553484',
+			from: auth.fromPhone,
+			body: "Hi there " + req.body.request.intent.slots.textTo.value + "!"
+		},
+		function(err){
+			err ? res.status(500).send("I'm sorry, but something went wrong") : res.send({
+				response: {
+					outputSpeech: {
+						type: "PlainText",
+						text: "Hello has been sent"
+					}
+				}
+			});
+		}
+	);
 });
 
 app.listen(9000);

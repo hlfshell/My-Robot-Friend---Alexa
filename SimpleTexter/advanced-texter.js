b2d72dd7d98785304e4d5831a553af1ef70d89ce
Keith Chester <kchester@gmail.com>
2016-08-06 22:49:55 -0500

    feature: Got custom slots and advanced texter example done and working

--- SimpleTexter/advanced-texter.js
+++ SimpleTexter/advanced-texter.js
@@ -12,25 +12,42 @@ var numbers = require('./numbers.json');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use(function(req, res){
-	console.log(req.body);
+var sendMessage = function sendMessage(to, msg, res){
+	console.log(to, numbers, numbers[to]);
 	twilio.sendMessage(
 		{
-			to: numbers[req.body.intent.slots.textTo.value],
+			to: numbers[to.toLowerCase()],
 			from: auth.fromPhone,
-			body: "Hi there " + req.body.request.intent.slots.textTo.value + "!"
+			body: msg
 		},
 		function(err){
+			console.log("sent text", err);
 			err ? res.status(500).send("I'm sorry, but something went wrong") : res.send({
 				response: {
 					outputSpeech: {
 						type: "PlainText",
-						text: "Hello has been sent"
+						text: "Message has been sent"
 					}
 				}
 			});
 		}
 	);
+}
+
+app.use(function(req, res){
+	console.log(req.body);
+	
+	switch(req.body.request.intent.name){
+		case "SayHelloIntent":
+			sendMessage(req.body.request.intent.slots.textTo.value, "Hi there " + req.body.request.intent.slots.textTo.value + "!", res);
+			break;
+		case "SendMessageIntent":
+			sendMessage(req.body.request.intent.slots.textTo.value, req.body.request.intent.slots.msg.value, res);
+			break;
+		default:
+			res.status(500).send("I don't recognize which intent you're trying to trigger");
+	}
+	
 });
 
 app.listen(9000);
